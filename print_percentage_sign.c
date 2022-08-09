87ec47879d352fa625f071b73ad3dac9dc012a5e
Toka-1992 <t.elwetedy@gmail.com>
2022-08-09 06:10:38 -0700

    Print a percentage sign

---
+++ print_percentage_sign.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_percent - print %
+* @ap: va_list
+* Return: number of printed characters
+*/
+int printf_percent(va_list ap)
+{
+printf("%%");
+return (1);
+}
