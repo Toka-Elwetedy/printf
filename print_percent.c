018909ea69bc6408b9ea33f0d780345cd89cb947
Toka-1992 <t.elwetedy@gmail.com>
2022-08-09 08:09:32 -0700

    Another way to print a percentage sign

---
+++ print_percent.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_percent - Prints a percent to stdout
+* @ap: va_list
+*
+* Return: The number of characters printed
+*/
+int printf_percent(va_list ap)
+{
+_putchar('%');
+return (1);
+}
